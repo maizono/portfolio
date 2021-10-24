7ce7c94033a74c359d5644deef28a7472eb16bac
maizono <maizono@github.com>
2021-10-24 14:30:18 -0400

    Huge update

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();   
+});
+
+function resBtn(){
+	window.open(
+		'documents/alugo_resume.pdf',
+		'_blank'
+	);
+}
+
+//alert("The file is working.");
+
