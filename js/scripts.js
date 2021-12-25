0e934ff445a866ec9922bdd43515bb3b2d975453
maizono <maizono@github.com>
2021-12-24 22:41:30 -0500

    cchange

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,10 @@ document.getElementById("btn1").addEventListener("click", displayModal);
 document.getElementById("btn1").addEventListener("click", btn1Content);
 
 function btn1Content(){
-	document.getElementById("modalText").innerHTML = "<a href='https://www.google.com'>Click me</a>";
+	document.getElementById("modalTitle").innerHTML = "My First Website";
+	document.getElementById("modalText").innerHTML = "I made this website way back in my freshman year of college. At the time, I only really knew basic HTML and CSS.<br>It's quite outdated as well; it has a flash movie on the homepage. It's a little embarrassing, but we all start somewhere.";
+	document.getElementById("modalImg").innerHTML = "<img src='images/web1.jpg'>";
+	document.getElementById("modalLink").innerHTML = "<a href='https://maizono.github.io/portfolio/works/web1'>View Live Example</a>";
 }
 
 document.getElementById("btn2").addEventListener("click", displayModal);
